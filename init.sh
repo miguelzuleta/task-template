931dda3b79d122a1d793d792540911f778a6c05f
Miguel Zuleta <mzuleta@apple.com>
2017-09-05 23:36:10 -0700

    copy dev files and push new repo master

--- init.sh
+++ init.sh
@@ -1,2 +1,22 @@
 echo "what's the epo name?"
 read projectName
+
+git checkout --track origin/dev
+mkdir ../temp-init
+cp -r . ../temp-init
+
+function deleteTempGit() {
+  cd ../temp-init
+  rm -rf .git
+}
+deleteTempGit
+
+git checkout master
+rm -rf .git
+
+git init
+git add .
+git commit -m “init master branch”
+
+git remote add origin https://github.com/miguelzuleta/$projectName.git
+git push -u origin master
