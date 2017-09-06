6b92025d7f85b6e47cdabc9f47edc4c6f67a93bb
miguelzuleta <zzuleta@hotmail.com>
2017-09-06 13:52:06 -0700

    delete init shell on first merge

--- merge.sh
+++ merge.sh
@@ -1,10 +1,22 @@
 # chmod +x ./merge.sh
 
+function deleteInit() {
+	if [ -e init.sh ]
+	then
+		rm -rf init.sh
+	fi
+}
+
+deleteInit
+
 gulp --prod --merge
 git add .
 git commit -m 'merge'
 git push
 git checkout master
+
+deleteInit
+
 git checkout dev site
 mv site/* .
 rm -rf site
