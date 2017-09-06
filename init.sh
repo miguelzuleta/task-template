4347d347d069a2c5511528fcdcc1c730ddcf95cc
Miguel Zuleta <mzuleta@apple.com>
2017-09-06 11:12:17 -0700

    add comments to shell

--- init.sh
+++ init.sh
@@ -4,9 +4,13 @@ echo "what's the epo name?"
 read projectName
 
 git checkout --track origin/dev
+
+# create temp-init folder
+# to copy dev contents
 mkdir ../$tempfolder
 cp -r . ../$tempfolder
 
+# handle master branch
 git checkout master
 rm -rf .git
 
@@ -17,8 +21,11 @@ git commit -m "init master branch"
 git remote add origin https://github.com/miguelzuleta/$projectName.git
 git push -u origin master
 
-
+# handle dev branch
 git checkout -b dev
+
+# add contents from temp-init folder
+# into current dev branch
 rm -rf ../$tempfolder/.git
 mv ../$tempfolder/{.,}* ./
 rm -rf ../$tempfolder
