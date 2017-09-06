e67049eec4413a49df5a74b1316a830fc931cac4
Miguel Zuleta <mzuleta@apple.com>
2017-09-05 23:58:30 -0700

    init test: successfully create and push new master branch

--- init.sh
+++ init.sh
@@ -5,18 +5,12 @@ git checkout --track origin/dev
 mkdir ../temp-init
 cp -r . ../temp-init
 
-function deleteTempGit() {
-  cd ../temp-init
-  rm -rf .git
-}
-deleteTempGit
+git checkout master
+rm -rf .git
 
-# git checkout master
-# rm -rf .git
+git init
+git add .
+git commit -m "init master branch"
 
-# git init
-# git add .
-# git commit -m "init master branch"
-
-# git remote add origin https://github.com/miguelzuleta/$projectName.git
-# git push -u origin master
+git remote add origin https://github.com/miguelzuleta/$projectName.git
+git push -u origin master
