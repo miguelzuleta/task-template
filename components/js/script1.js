8a46f86070a4246e804e136641b13252044f4036
mzuleta <zzuleta@hotmail.com>
2017-10-07 21:51:18 -0700

    simplify init files

--- components/js/script1.js
+++ components/js/script1.js
@@ -1,17 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-function func1(...args){
- 
-	args.forEach((element, index) => {
-		console.log(`${index + 1} → ${element / 7}`)
-		console.log(element)
-	})
-
-}
-func1(3, 511, 6, 2, 21)
-
 ReactDOM.render(
-  <h1>magic!!!</h1>,
-  document.getElementById('root')
+  <span>React Render</span>,
+  document.getElementById('react-render')
 )
